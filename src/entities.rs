586bd3e68631fdd9f948b00c94131d96d3db306d
Przemyslaw Kleszcz <kleszcz@mail.com>
2022-01-14 17:17:50 +0100

    game entities defined

---
+++ src/entities.rs
@@ -0,0 +1,24 @@
+use crate::components::*;
+use specs::{Builder, World, WorldExt};
+
+pub fn create_wall(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/wall.png".to_string(),
+        })
+        .with(Immovable)
+        .build();
+}
+
+pub fn create_player(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/player.png".to_string(),
+        })
+        .with(Movable)
+        .build();
+}
