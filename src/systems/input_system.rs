e5c527f2e57ccdac7b935a2a4503adbba539db7a
Przemyslaw Kleszcz <kleszcz@mail.com>
2022-01-15 23:53:41 +0100

    input system modified to move player in the proper interval

--- src/systems/input_system.rs
+++ src/systems/input_system.rs
@@ -1,16 +1,52 @@
-use specs::{System, Write};
+use ggez::event::KeyCode;
+use ggez::Context;
+use specs::{Entities, Join};
+use specs::{ReadStorage, System, Write, WriteStorage};
 
+use crate::components::*;
 use crate::resources::*;
 
-pub struct InputSystem {}
+pub struct InputSystem<'a> {
+    pub context: &'a mut Context,
+}
 
-impl<'a> System<'a> for InputSystem {
-    type SystemData = Write<'a, InputQueue>;
+pub const MOVE_TICK_INTERVAL: u32 = 15;
+
+impl<'a> System<'a> for InputSystem<'a> {
+    type SystemData = (
+        Write<'a, InputQueue>,
+        Entities<'a>,
+        WriteStorage<'a, Position>,
+        ReadStorage<'a, Player>,
+    );
 
     fn run(&mut self, data: Self::SystemData) {
-        let mut input_queue = data;
-        if let Some(key) = input_queue.keys_pressed.pop() {
-            println!("Steps in the queue: {:?}", key);
+        let (mut input_queue, entities, mut positions, players) = data;
+
+        let ticks = ggez::timer::ticks(self.context) as u32;
+        if ticks <= input_queue.next_move_at {
+            return;
+        }
+
+        input_queue.next_move_at = ticks + MOVE_TICK_INTERVAL;
+
+        for (_player, entity) in (&players, &entities).join() {
+            if let Some(key) = input_queue.key_pressed {
+                let position = positions.get_mut(entity);
+                update_position(position, key);
+            }
+        }
+    }
+}
+
+fn update_position(position: Option<&mut Position>, key: KeyCode) {
+    if let Some(position) = position {
+        match key {
+            KeyCode::Up => position.y -= 1,
+            KeyCode::Down => position.y += 1,
+            KeyCode::Left => position.x -= 1,
+            KeyCode::Right => position.x += 1,
+            _ => (),
         }
     }
 }
